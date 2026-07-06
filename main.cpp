221d945b876bf87e0a3225c163ebbfcdadd690c5
slava-zotc <slavasoc4@gmail.com>
2026-07-06 14:43:02 +0400

    Реализован основной цикл обработки соединений и отправка HTTP-ответов

--- main.cpp
+++ main.cpp
@@ -2,14 +2,79 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <vector>
+#include <poll.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void accept_connection()
+{
+}
+
+void core_loop(int server_fd)
+{
+    std::vector<struct pollfd> fds(1);
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;
+    struct pollfd tmp_pollfd;
+    int ret;
+
+    while (true)
+    {
+        ret = poll(fds.data(), fds.size(), 1000);
+
+        if (ret < 0)
+        {
+            std::cerr << "ERROR Poll failed" << std::endl;
+            return;
+        }
+        if (ret == 0)
+        {
+            continue;
+        }
+
+        if (fds[0].revents & POLLIN)
+        {
+
+            int fd_client = accept(server_fd, NULL, NULL);
+
+            if (fd_client < 0)
+                return; //! TODO Handke error
+            if (fcntl(fd_client, F_SETFL, O_NONBLOCK) == -1)
+                return; // handle error
+            tmp_pollfd.fd = fd_client;
+            tmp_pollfd.events = POLLIN;
+
+            fds.push_back(tmp_pollfd);
+        }
+
+        for (size_t i = 1; i < fds.size() && ret != 0; i++)
+        {
+            if (fds[i].revents & POLLIN)
+            {
+                char tmp_buffer[4096];
+                std::string mesage = "";
+                int byte_recv = recv(fds[i].fd, tmp_buffer, sizeof(tmp_buffer), 0);
+                mesage.append(tmp_buffer, byte_recv);
+                std::cout << mesage << std::endl;
+                std::string response("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+                int byte_send = send(fds[i].fd, response.c_str(), response.size(), 0);
+                if (byte_send == -1)
+                    return; //! TODO Handke error
+                close(fds[i].fd);
+                fds.erase(fds.begin() + i);
+                i--;
+            }
+        }
+    }
+}
 
 int main()
 {
     int server_fd = socket(PF_INET, SOCK_STREAM, 0);
-
     if (server_fd == -1)
         return 1;
-
+    if (fcntl(server_fd, F_SETFL, O_NONBLOCK) == -1)
+        return -1;
     struct sockaddr_in adrr;
     adrr.sin_family = PF_INET;
     adrr.sin_port = htons(8080);
@@ -22,25 +87,5 @@ int main()
 
     std::cout << "SErver listening on port 8080" << std::endl;
 
-    struct sockaddr_in client_address;
-    socklen_t adrr_size = sizeof(client_address);
-    std::vector<int> client_fd;
-    int tmp_fd;
-    while (true)
-    {
-        tmp_fd = accept(server_fd, (struct sockaddr *)&client_address, &adrr_size);
-        if (tmp_fd < 0)
-            std::cout << "HANDLE ERROR" << std::endl;
-        client_fd.push_back(tmp_fd);
-        std::cout << client_fd.back() << std::endl;
-        char temp_bufer[4096];
-        std::string mesage = "";
-        int byte_recv = recv(tmp_fd, temp_bufer, sizeof(temp_bufer), 0);
-        mesage.append(temp_bufer, byte_recv);
-        std::cout << mesage << std::endl;
-        std::string response("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
-        int byte_send = send(tmp_fd, response.c_str(), response.size(), 0);
-        if (byte_send == -1)
-            return -1;
-    }
+    core_loop(server_fd);
 }
