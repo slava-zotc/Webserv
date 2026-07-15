b8b3f23af94086dd7fe82080c4d7ee3ceaa47ab2
slava-zotc <slavasoc4@gmail.com>
2026-07-15 17:28:30 +0400

    feat(http): реализовать парсинг заголовков и тела HTTP-запроса
    - Добавлена функция process_header_line для разбора заголовков и извлечения Content-Length.
    - Реализовано состояние PARSING_BODY в методе parse для корректного чтения тела запроса.
    - Добавлена временная заглушка (TODO) для обработки chunked transfer-encoding.

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -1,55 +1,54 @@
 #ifndef HTTP_REQUEST_HPP
 #define HTTP_REQUEST_HPP
 
-#include <string>
 #include <map>
-#include <sstream>
+#include <string>
 
 class HttpRequest
 {
 public:
-    enum Methods
-    {
-        GET,
-        POST,
-        DELETE,
-        UNKNOWN
-    };
-    enum ParsingState
-    {
-        PARSING_START,
-        PARSING_HEADERS,
-        PARSING_BODY,
-        PARSING_DONE,
-        PARSING_ERROR
-    };
+	enum Methods
+	{
+		GET,
+		POST,
+		DELETE,
+		UNKNOWN
+	};
+	enum ParsingState
+	{
+		PARSING_START,
+		PARSING_HEADERS,
+		PARSING_BODY,
+		PARSING_DONE,
+		PARSING_ERROR
+	};
 
-    HttpRequest();
-    ParsingState parse(const std::string &data);
-    const std::string &get_path() const;
-    const std::string &get_version() const;
-    const std::string &get_body() const;
-    const std::map<std::string, std::string> &get_headers() const;
-    Methods get_method() const;
-    ParsingState get_parsing_state() const;
-    ~HttpRequest();
+	HttpRequest();
+	ParsingState parse(const std::string& data);
+	const std::string& get_path() const;
+	const std::string& get_version() const;
+	const std::string& get_body() const;
+	const std::map<std::string, std::string>& get_headers() const;
+	Methods get_method() const;
+	ParsingState get_parsing_state() const;
+	~HttpRequest();
 
 private:
-    std::string internal_buffer_;
-    Methods method_;
-    ParsingState parsing_state_;
-    std::string path_;
-    std::string version_;
-    std::string body_;
-    std::map<std::string, std::string> headers_;
-    size_t content_length_;
+	std::string internal_buffer_;
+	Methods method_;
+	ParsingState parsing_state_;
+	std::string path_;
+	std::string version_;
+	std::string body_;
+	std::map<std::string, std::string> headers_;
+	size_t content_length_;
 
-    void process_start_line(const std::string &line);
-    Methods convert_method_str(const std::string &method_str);
+	void process_start_line(const std::string& line);
+	Methods convert_method_str(const std::string& method_str);
 
-    void process_header_line(const std::string &line);
-    HttpRequest(const HttpRequest &src);
-    HttpRequest &operator=(const HttpRequest &rhs);
+	void process_header_line(const std::string& line);
+	HttpRequest(const HttpRequest& src);
+	HttpRequest& operator=(const HttpRequest& rhs);
 };
 
-#endif // HTTP_REQUEST_HPP
+#endif	// HTTP_REQUEST_HPP
--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -1,95 +1,142 @@
 #include "HttpRequest.hpp"
 
-HttpRequest::HttpRequest(void) : internal_buffer_(),
-                                 method_(UNKNOWN),
-                                 parsing_state_(PARSING_START),
-                                 path_(),
-                                 version_(),
-                                 body_(),
-                                 headers_(),
-                                 content_length_(0)
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+#include <string>
+
+HttpRequest::HttpRequest(void)
+	: internal_buffer_(),
+	  method_(UNKNOWN),
+	  parsing_state_(PARSING_START),
+	  path_(),
+	  version_(),
+	  body_(),
+	  headers_(),
+	  content_length_(0)
 {
 }
 
-HttpRequest::Methods HttpRequest::convert_method_str(const std::string &method_str)
+HttpRequest::Methods HttpRequest::convert_method_str(
+	const std::string& method_str)
 {
-
-    if (method_str == "GET")
-    {
-        return GET;
-    }
-    else if (method_str == "POST")
-    {
-        return POST;
-    }
-    else if (method_str == "DELETE")
-    {
-        return DELETE;
-    }
-    else
-    {
-        return UNKNOWN;
-    }
+	if (method_str == "GET")
+	{
+		return GET;
+	}
+	else if (method_str == "POST")
+	{
+		return POST;
+	}
+	else if (method_str == "DELETE")
+	{
+		return DELETE;
+	}
+	else
+	{
+		return UNKNOWN;
+	}
+}
+void HttpRequest::process_header_line(const std::string& line)
+{
+	std::string key;
+	std::string value;
+	if (line.empty())
+	{
+		if (headers_.count("content-length") > 0)
+		{
+			std::stringstream ss(headers_["content-length"]);
+			long long tmp_content_length;
+			ss >> tmp_content_length;
+			if (ss.fail() || !ss.eof() || tmp_content_length < 0)
+			{
+				parsing_state_ = PARSING_ERROR;
+				return;
+			}
+			content_length_ = static_cast<size_t>(tmp_content_length);
+			if (content_length_ > 0)
+				parsing_state_ = PARSING_BODY;
+			else
+				parsing_state_ = PARSING_DONE;
+		}
+		else
+		{
+			parsing_state_ = PARSING_DONE;
+		}
+		// TODO Написать обработчик для chanked transfer-encoding
+		return;
+	}
+	size_t pos = line.find(':');
+	if (pos == std::string::npos)
+	{
+		parsing_state_ = PARSING_ERROR;
+		return;
+	}
+	key = line.substr(0, pos);
+	std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+	value = line.substr(pos + 1);
+	size_t first_not_space = value.find_first_not_of("\t ");
+	if (first_not_space == std::string::npos)
+	{
+		value = "";
+	}
+	else
+	{
+		value = value.substr(first_not_space);
+	}
+	headers_[key] = value;
 }
 
-void HttpRequest::process_header_line(const std::string &line){
-    std::string key;
-    std::string value;
+void HttpRequest::process_start_line(const std::string& line)
+{
+	std::stringstream ss(line);
+	std::string method_str, path_str, version_str;
 
-    size_t pos = line.find(':');
-    if (pos == std::string::npos)
-    {
-        parsing_state_ = PARSING_ERROR;
-    }
-    key = line.substr(0, pos);
-    value
+	ss >> method_str >> path_str >> version_str;
+	if (method_str.empty() || path_str.empty() || version_str.empty())
+	{
+		parsing_state_ = PARSING_ERROR;
+		return;
+	}
+	method_ = convert_method_str(method_str);
+	path_ = path_str;
+	version_ = version_str;
+	parsing_state_ = PARSING_HEADERS;
 }
 
-void HttpRequest::process_start_line(const std::string &line)
+HttpRequest::ParsingState HttpRequest::parse(const std::string& data)
 {
-    std::stringstream ss(line);
-    std::string method_str, path_str, version_str;
+	internal_buffer_ += data;
 
-    ss >> method_str >> path_str >> version_str;
-    if (method_str.empty() || path_str.empty() || version_str.empty())
-    {
-        parsing_state_ = PARSING_ERROR;
-        return;
-    }
-    method_ = convert_method_str(method_str);
-    path_ = path_str;
-    version_ = version_str;
-    parsing_state_ = PARSING_HEADERS;
-}
+	while (parsing_state_ == PARSING_START || parsing_state_ == PARSING_HEADERS)
+	{
+		size_t pos = internal_buffer_.find("\r\n");
+		if (std::string::npos == pos)
+		{
+			break;
+		}
+		std::string line = internal_buffer_.substr(0, pos);
+		internal_buffer_.erase(0, pos + 2);
+		if (parsing_state_ == PARSING_START)
+		{
+			if (!line.empty()) process_start_line(line);
+		}
+		else if (parsing_state_ == PARSING_HEADERS)
+		{
+			process_header_line(line);
+		}
+	}
+	if (parsing_state_ == PARSING_BODY)
+	{
+		size_t need_byte = content_length_ - body_.size();
+		size_t to_copy = std::min(internal_buffer_.size(), need_byte);
 
-HttpRequest::ParsingState HttpRequest::parse(const std::string &data)
-{
-    internal_buffer_ += data;
+		body_.append(internal_buffer_, 0, to_copy);
+		internal_buffer_.erase(0, to_copy);
+		if (body_.size() == content_length_) parsing_state_ = PARSING_DONE;
+	}
 
-    while (parsing_state_ == PARSING_START || parsing_state_ == PARSING_HEADERS)
-    {
-        size_t pos = internal_buffer_.find("\r\n");
-        if (std::string::npos == pos)
-        {
-            break;
-        }
-        std::string line = internal_buffer_.substr(0, pos);
-        internal_buffer_.erase(0, pos + 2);
-        if (parsing_state_ == PARSING_START)
-        {
-            if (!line.empty())
-                process_start_line(line);
-        }
-        else if (parsing_state_ == PARSING_HEADERS)
-        {
-            process_header_line(line);
-        }
-    }
-    if (parsing_state_ == PARSING_BODY)
-    {
-    }
-
-    return parsing_state_;
+	return parsing_state_;
 }
 
 HttpRequest::~HttpRequest(void)
